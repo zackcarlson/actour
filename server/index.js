433c5808ab1b0726d2b68925b0cc9637e52c0f8b
zackcarlson <zackt.carlson@gmail.com>
2020-08-15 21:58:07 -0700

    Add heroku keys

--- server/index.js
+++ server/index.js
@@ -4,11 +4,10 @@ const cors = require("cors");
 const path = require("path");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema");
-
 const app = express();
+
 app.use(cors());
 app.use(express.static(path.join(__dirname, "../client/build")));
-
 app.use(
   "/graphql",
   graphqlHTTP({
