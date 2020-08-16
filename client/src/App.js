c57cbb9dbc23f92e72df2ae1b8994df92de4e575
zackcarlson <zackt.carlson@gmail.com>
2020-08-16 16:08:28 -0700

    Add Routes and Apollo

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,12 @@
 import React from "react";
-import { ApolloProvider } from "react-apollo";
-import { ApolloClient } from "apollo-client";
-import { HttpLink } from "apollo-link-http";
-import { InMemoryCache } from "apollo-cache-inmemory";
-import Landing from "./pages/landing";
 import "./App.css";
-
-const httpLink = new HttpLink({
-  uri: "/graphql",
-});
-
-const client = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache(),
-});
+import Routes from "./routes";
 
 function App() {
   return (
-    <ApolloProvider client={client}>
-      <div className="App">
-        <Landing />
-      </div>
-    </ApolloProvider>
+    <div className="App--container">
+      <Routes />
+    </div>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import { ApolloProvider } from "react-apollo";
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import "./index.css";
+
+const httpLink = new HttpLink({
+  uri: "/graphql",
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </Router>,
   document.getElementById("root")
 );
 
