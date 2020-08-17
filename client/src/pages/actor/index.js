5205f43ea61223e43420adf41dc697206e1dbfc6
Mealear <mileyy.mk@gmail.com>
2020-08-16 19:47:04 -0700

    Adds GraphQL queries to Actor page

--- client/src/pages/actor/index.js
+++ client/src/pages/actor/index.js
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { withRouter, useParams } from "react-router";
 import { withApollo } from "react-apollo";
-import { GET_ACTOR } from "../../queries";
+import { GET_ACTOR, GET_ACTOR_IMDB_ID, GET_ACTOR_AWARDS, GET_ACTOR_CREDITS } from "../../queries";
 import "./index.css";
 
 const Actor = (props) => {
   const { name } = useParams();
-  const [actorStats, setActorStats] = useState(null);
+  const [actorInfo, setActorInfo] = useState(null);
 
   useEffect(() => {
-    getActorStats();
+    handleQueries();
   }, []);
 
   const getActorStats = async () => {
@@ -18,20 +18,59 @@ const Actor = (props) => {
       query: GET_ACTOR,
       variables: { query: name },
     });
-    setActorStats(data.actor);
+    return data.actor;
   };
 
-  console.log("stats", actorStats);
+  const getActorImdbId = async (id) => {
+    const { client } = props;
+    const { data } = await client.query({
+      query: GET_ACTOR_IMDB_ID,
+      variables: { id },
+    });
+    return data.actorDetails.imdb_id;
+  };
+
+  const getActorAwards = async (imdbID) => {
+    const { client } = props;
+    const { data } = await client.query({
+      query: GET_ACTOR_AWARDS,
+      variables: { id: imdbID },
+    });
+    return data.actorAwards.awardsSummary;
+  };
+
+  const getActorCredits = async (imdbID) => {
+    const { client } = props;
+    const { data } = await client.query({
+      query: GET_ACTOR_CREDITS,
+      variables: { id: imdbID },
+    });
+    return data.actorCredits.filmography;
+  }
+
+  const handleQueries = async () => {
+    const stats = await getActorStats();
+    const imdbID = await getActorImdbId(stats.id);
+    const awards = await getActorAwards(imdbID);
+    const credits = await getActorCredits(imdbID);
+    setActorInfo({
+      stats,
+      awards,
+      credits
+    });
+  };
+
+  console.log('actor info', actorInfo)
   return (
     <div className="Actor--container">
       <div className="Actor--stats">
         <div className="Actor--image">
           <img
-            src={actorStats && actorStats.profile_path}
+            src={actorInfo && actorInfo.stats.profile_path}
             alt={`${name}'s avatar`}
           />
         </div>
-        <div className="Actor--name">{actorStats && actorStats.name}</div>
+        <div className="Actor--name">{actorInfo && actorInfo.stats.name}</div>
 
         <div className="Actor--highlights">
           <div className="Actor--movie-count"></div>
@@ -41,8 +80,8 @@ const Actor = (props) => {
 
       <div className="Actor--notable-work">
         {/* Map through a list of NotableWork components */}
-        {actorStats &&
-          actorStats.known_for.map(({ poster_path, title }, i) => {
+        {actorInfo &&
+          actorInfo.stats.known_for.map(({ poster_path, title }, i) => {
             return (
               <div
                 className="Actor--notable-credit"
