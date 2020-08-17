2b5b13ac3611fe00671cedc205fb479e9aaf02bf
zackcarlson <zackt.carlson@gmail.com>
2020-08-16 18:16:49 -0700

    Adds actor data to Actor page

--- client/src/pages/actor/index.js
+++ client/src/pages/actor/index.js
@@ -1,14 +1,37 @@
-import React from "react";
-import { withRouter } from "react-router";
+import React, { useState, useEffect } from "react";
+import { withRouter, useParams } from "react-router";
 import { withApollo } from "react-apollo";
+import { GET_ACTOR } from "../../queries";
 import "./index.css";
 
 const Actor = (props) => {
+  const { name } = useParams();
+  const [actorStats, setActorStats] = useState(null);
+
+  useEffect(() => {
+    getActorStats();
+  }, []);
+
+  const getActorStats = async () => {
+    const { client } = props;
+    const { data } = await client.query({
+      query: GET_ACTOR,
+      variables: { query: name },
+    });
+    setActorStats(data.actor);
+  };
+
+  console.log("stats", actorStats);
   return (
     <div className="Actor--container">
       <div className="Actor--stats">
-        <div className="Actor--image"></div>
-        <div className="Actor--name"></div>
+        <div className="Actor--image">
+          <img
+            src={actorStats && actorStats.profile_path}
+            alt={`${name}'s avatar`}
+          />
+        </div>
+        <div className="Actor--name">{actorStats && actorStats.name}</div>
 
         <div className="Actor--highlights">
           <div className="Actor--movie-count"></div>
@@ -18,6 +41,17 @@ const Actor = (props) => {
 
       <div className="Actor--notable-work">
         {/* Map through a list of NotableWork components */}
+        {actorStats &&
+          actorStats.known_for.map(({ poster_path, title }, i) => {
+            return (
+              <div
+                className="Actor--notable-credit"
+                key={`${i} ${poster_path}`}
+              >
+                <img src={poster_path} alt="Movie Poster" title={title} />
+              </div>
+            );
+          })}
       </div>
 
       <div className="Actor--credits">
