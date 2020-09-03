dcbc2ce287e914ae0434eadaf28270e84090ee3c
zackcarlson <zackt.carlson@gmail.com>
2020-09-03 14:01:52 -0700

    Removes page transitions

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,18 +1,13 @@
 import React from "react";
-import { CSSTransition, TransitionGroup } from "react-transition-group";
-import { Route, Switch, withRouter } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Landing from "../pages/landing";
 import Actor from "../pages/actor";
 
-const Routes = withRouter(({ location }) => (
-  <TransitionGroup>
-    <CSSTransition key={location.key} classNames="slide" timeout={1000}>
-      <Switch location={location}>
-        <Route exact path="/" component={Landing} />
-        <Route path="/actor/:name/:imdb" component={Actor} />
-      </Switch>
-    </CSSTransition>
-  </TransitionGroup>
-));
+const Routes = () => (
+  <Switch>
+    <Route exact path="/" component={Landing} />
+    <Route path="/actor/:name/:imdb" component={Actor} />
+  </Switch>
+);
 
 export default Routes;
