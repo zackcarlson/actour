8e064c7071274d41ede154011bd618326a3a4ad3
zackcarlson <zackt.carlson@gmail.com>
2020-09-04 23:25:14 -0700

    Tests empty Avatar image

--- client/src/components/avatar/__tests__/avatar.test.js
+++ client/src/components/avatar/__tests__/avatar.test.js
@@ -8,7 +8,11 @@ describe("Avatar component", () => {
   const { profile_path, name } = dummyData;
   it("should render an actor profile image", () => {
     const avatar = shallow(<Avatar actorInfo={{ profile_path, name }} />);
-
     expect(avatar.find("img").prop("src")).toEqual(profile_path);
+  });
+
+  it("should show alt text when image is not rendered", () => {
+    const avatar = shallow(<Avatar actorInfo={{ profile_path: "", name }} />);
+    expect(avatar.find("img").prop("alt")).toEqual(`${name}'s avatar`);
   });
 });
